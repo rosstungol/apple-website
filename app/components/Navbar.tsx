cf4b5a72c1218d7b227edf20cab13fbbc3e10998
Ross Tungol <ross@proudcloud.io>
2024-03-28 01:59:16 +0800

    Replaces Image with img

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image"
 import { navLists } from "../constants"
 
 export const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
       <nav className='flex w-full screen-max-width'>
-        <Image
-          src='/assets/images/apple.svg'
-          alt='Apple'
-          width='14'
-          height='18'
-        />
+        <img src='/assets/images/apple.svg' alt='Apple' />
 
         <div className='flex flex-1 justify-center max-sm:hidden'>
           {navLists.map((nav) => (
@@ -24,18 +18,8 @@ export const Navbar = () => {
         </div>
 
         <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
-          <Image
-            src='/assets/images/search.svg'
-            alt='search'
-            width='18'
-            height='18'
-          />
-          <Image
-            src='/assets/images/bag.svg'
-            alt='bag'
-            width='18'
-            height='18'
-          />
+          <img src='/assets/images/search.svg' alt='search' />
+          <img src='/assets/images/bag.svg' alt='bag' />
         </div>
       </nav>
     </header>
